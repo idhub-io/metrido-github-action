18c24adbb67010f5a97f6a409f4478f9afc57636
Quentin Castel <quentincastel86@gmail.com>
2022-02-10 09:17:28 +0100

    refactor

--- github-actions.sh
+++ github-actions.sh
@@ -8,14 +8,10 @@ export SOURCE="GITHUB"
 export AUTHOR="${GITHUB_ACTOR}"
 export COMMIT_MESSAGE="${{ github.event.head_commit.message }}"
 
-export COMMIT_MESSAGE="${{ github.event.head_commit.message }}"
+if [ -d "${STEP_STATUS}" ]; then
+  echo "Step status defined: ${STEP_STATUS}"
 
-if [ -d "${STEP_NAME}" ]; then
-  echo "Step name defined: ${STEP_NAME}"
-  step_outcome_var="steps.${STEP_NAME}.outcome"
-  echo "Var ${step_outcome_var}: ${!step_outcome_var}"
-
-  if [[ "${!step_outcome_var}" == "success" ]]; then
+  if [[ "${STEP_STATUS}" == "success" ]]; then
       export STATUS="SUCCEEDED"
   else
       export STATUS="FAILED"
