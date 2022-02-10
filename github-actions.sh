eec869ddb7247beb37c9a29f18cf2e944bfd16e4
Quentin Castel <quentincastel86@gmail.com>
2022-02-10 08:59:53 +0100

    Add step status

--- github-actions.sh
+++ github-actions.sh
@@ -8,4 +8,18 @@ export SOURCE="GITHUB"
 export AUTHOR="${GITHUB_ACTOR}"
 export COMMIT_MESSAGE="${{ github.event.head_commit.message }}"
 
+export COMMIT_MESSAGE="${{ github.event.head_commit.message }}"
+
+if [ -d "${STEP_NAME}" ]; then
+  echo "Step name defined: ${STEP_NAME}"
+  step_outcome_var="steps.${STEP_NAME}.outcome"
+  echo "Var ${step_outcome_var}: ${!step_outcome_var}"
+
+  if [[ "${!step_outcome_var}" == "success" ]]; then
+      export STATUS="SUCCEEDED"
+  else
+      export STATUS="FAILED"
+  fi
+fi
+
 entrypoint.sh
