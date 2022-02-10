73e1981e0849bd6d5580b325f21532fbf1d5430e
Quentin Castel <quentincastel86@gmail.com>
2022-02-10 09:26:15 +0100

    remove commit message env var

--- github-actions.sh
+++ github-actions.sh
@@ -6,7 +6,6 @@ export COMMIT_ID="${GITHUB_SHA}"
 export BRANCH="${GITHUB_REF_NAME}"
 export SOURCE="GITHUB"
 export AUTHOR="${GITHUB_ACTOR}"
-export COMMIT_MESSAGE="${{ github.event.head_commit.message }}"
 
 if [ -d "${STEP_STATUS}" ]; then
   echo "Step status defined: ${STEP_STATUS}"
