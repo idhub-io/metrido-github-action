21c02cfb139d21a715b63c82496a3514f0607efe
Quentin Castel <quentincastel86@gmail.com>
2022-02-10 08:13:06 +0100

    export Commit message by default

--- github-actions.sh
+++ github-actions.sh
@@ -6,6 +6,6 @@ export COMMIT_ID="${GITHUB_SHA}"
 export BRANCH="${GITHUB_REF_NAME}"
 export SOURCE="GITHUB"
 export AUTHOR="${GITHUB_ACTOR}"
-
+export COMMIT_MESSAGE="${{ github.event.head_commit.message }}"
 
 entrypoint.sh
